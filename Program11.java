4f58c602b72de89ddb282e6af18a2b5981d6ae1c
WithJass19 <jaspreetmehra185@gmail.com>
2023-09-14 22:20:55 +0530

    update Program

---
+++ Program11.java
@@ -0,0 +1,16 @@
+class CompareTwoNumbers{
+    public static void main(String args[]){
+        int num1 = 3;
+        int num2 = 5;
+
+        if(num1 > num2){
+            System.out.println(num1 + " greater than " + num2);
+        }
+        else if(num1 < num2){
+            System.out.println(num1 + " less than " + num2);
+        }
+        else{
+            System.out.println("equal numbers");
+        }
+    }
+}
