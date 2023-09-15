49d097b82473f216a27840eb310b57ea29482506
WithJass19 <jaspreetmehra185@gmail.com>
2023-09-15 17:43:11 +0530

    update

---
+++ Program27.java
@@ -0,0 +1,10 @@
+class CircleArea{
+    public static void main(String[] args)
+    {
+        int radius;
+        double pi = 3.142, area;
+        radius = 5;
+        area = pi * radius * radius;
+        System.out.println("Area of circle is :" + area);
+    }
+}
---
+++ Program28.java
@@ -0,0 +1,12 @@
+class AreaOfRectangle{
+   public static void main(String args[]) {  
+    int l= 5; 
+    System.out.println("The value of length: "+l);
+         
+    int b= 10;
+    System.out.println("The value of Breadth: "+b);
+         
+    int area=l*b;
+    System.out.println("Area of Rectangle is: " + area);   
+    }
+}
