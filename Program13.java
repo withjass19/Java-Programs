83e94aa8da877c0a1f20e815999cf25f0d271d91
Jaspreet <jaspreetmehra185@gmail.com>
2023-09-16 11:44:37 +0530

    Update Repo

---
+++ Program13.java
@@ -0,0 +1,9 @@
+class ASCII{
+    static int fuc(char s){
+        int num = s;
+        return num; 
+    }
+    public static void main(String a[]){
+        System.out.println(fuc('A'));
+    }
+}
