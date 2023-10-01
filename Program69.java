18f9fb0ffd4bd073695ba615e7330c6daa34e9b6
WithJass19 <jaspreetmehra185@gmail.com>
2023-10-01 23:18:04 +0530

    update repo

---
+++ Program69.java
@@ -0,0 +1,8 @@
+class ConcateTwoStrings{
+    public static void main(String args[]){
+        String str1 = "Jas",
+        str2 = "preet",
+        str3 = str1.concat(str2);
+        System.out.println(str3);
+    }
+}
---
+++ Program70.java
@@ -0,0 +1,7 @@
+class LowercaseString{
+    public static void main(String args[]){
+        String str = "JASPREET SINGH";
+        String ans = str.toLowerCase();
+        System.out.println(ans);
+    }
+}
